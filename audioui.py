1f925b4cea64c1a6664fd40b34383fc69a3fe008
jjustinllee <justin.lee1134@gmail.com>
2024-12-08 22:00:07 -0500

    Update audioui.py

--- audioui.py
+++ audioui.py
@@ -1,19 +1,19 @@
 import streamlit as st
-import fourierBackend as fb
+import fourier_backend as fb
 
 st.header("Preset Buttons")
 st.subheader("Each button will run a different preset")
 st.write("---")
 
-audioVal = st.audio_input("Record")
-if audioVal:
+audio_val = st.audio_input("Record")
+if audio_val:
     meow = st.audio(audioVal)
 
 emale = st.checkbox(label = "Eliminate Male Voices")
 efem = st.checkbox(label = "Eliminate Female Voices")
 def presets():
     if emale:
-        fb.elimMale()
+        fb.elim_male()
     if efem:
-        fb.elimFemale()
+        fb.elim_fem()
     
