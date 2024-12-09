8701b0b4e86b032fd8ba6b89477c0bef717cade5
jjustinllee <justin.lee1134@gmail.com>
2024-12-08 21:51:46 -0500

    Add files via upload

---
+++ audioui.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.header("Preset Buttons")
+st.subheader("Each button will run a different preset")
+st.write("---")
+
+audioVal = st.audio_input("Record")
+if audioVal:
+    st.audio(audioVal)
+
+emale = st.checkbox(label = "Eliminate Male Voices")
+efem = st.checkbox(label = "Eliminate Female Voices")
+def presets():
+    if emale:
+        elimMale()
+    if efem:
+        elimFemale()
+    
