b68e2d8be604efa4d3ea83abbe9a24e48433477d
Rasmit Devkota <rdatch101@gmail.com>
2024-12-09 19:53:34 -0500

    restructure

--- pages/Presets.py
+++ pages/Presets.py
@@ -1,12 +1,11 @@
 import streamlit as st
 
-from src.fourier_backend import fourier
+from fourier_backend import fourier
 
 st.header("Presets for Our Fourier Synth")
 audio_val = st.audio_input("Record")
 if audio_val:
     st.audio(audio_val)
-
 
 st.button("Process", type="primary", on_click=fourier)
 
